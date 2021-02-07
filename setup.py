b1bff93a48833c9e4ec6d79c4fa2ddea5315aa71
siddhesh <siddhesh.gore@mobpro.com>
2021-02-07 02:03:28 +0100

    Update new taf in setup.py file.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-db-storage2',
-    version='2.0.0',
+    version='2.0.1',
     url='https://github.com/hitman711/django-db-storage',
     license='BSD',
     author='Siddhesh Gore',
